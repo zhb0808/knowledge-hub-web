5fb7ad438916a50c70c7911502c80b2b861c6109
zhanghuibin <18531122171@163.com>
2026-09-01 18:04:34 +0800

    补传完整的 package-lock.json

--- src/views/WelcomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import { authStore } from '../stores/auth'
-</script>
-
-<template>
-  <div class="welcome-line">欢迎使用企业知识库，{{ authStore.user.value?.displayName }}。</div>
-</template>
